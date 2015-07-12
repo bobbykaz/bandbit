212649f0540222127cc299d7ab4161d18b7558e8
bobby kaz <robert.kazimiroff@gmail.com>
2015-07-11 23:25:38 -0700

    Attempting to fix nuget versioning issues, adding a console project so I can test out api interations without needing to load an emulator.

---
+++ Fitband.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitBand.Developer.Secrets;
+using System.IO;
+
+namespace Fitband.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fitbit.API.Client.FitbitClient client = new Fitbit.API.Client.FitbitClient(Secrets.ClientID, Secrets.ClientConsumerSecret, Secrets.CallbackURI);
+            //var token = client.Authenticate(code, Secrets.CallbackURI).ConfigureAwait(false).GetAwaiter().GetResult();
+            var token = client.Authenticate(Secrets.TestRefreshToken).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            Console.WriteLine("Done");
+
+        }
+    }
+}
